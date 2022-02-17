a58c024455359fce742bae9470753078961e89eb
Emil <Starostin.emil@mail.ru>
2022-02-17 17:36:40 +0300

    If need progect

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Init
+            Random rnd = new Random();
+            int[] n = new int[20];
+           
+            for(int i = 0; i < n.Length; i++)
+            {
+                n[i] = rnd.Next(0, 9);
+            }
+            Array.Sort(n);
+            //copy array
+            int[] nn = new int[n.Length];
+            for (int i = 0; i < n.Length; i++)
+            {
+                nn[i] = n[i];
+            }
+            //take pair
+            for (int i = 0; i < n.Length-1; i++)
+            {
+                if (nn[i] == nn[i + 1])
+                {
+                    nn[i] = Int32.MinValue;
+                    nn[i+1] = Int32.MinValue;
+                }  
+                
+            }
+            //output
+            for (int i = 0; i < nn.Length ; i++)
+            {
+                if (nn[i] != Int32.MinValue)
+                {
+                    Console.WriteLine(nn[i]);
+                }
+
+            }
+            Console.ReadKey();
+
+            
+
+            
+        }
+    }
+}
